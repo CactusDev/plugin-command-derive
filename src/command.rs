216d31dd0f588ca065f058fc57f9a035911a2c98
Innectic <innectic@gmail.com>
2018-04-23 22:40:55 -0700

    Revert "Return the command name when we're done"
    This reverts commit a88d5f4d21eadb38f9db94525481e3801da303da.

--- src/command.rs
+++ src/command.rs
@@ -2,18 +2,16 @@
 use syn;
 use quote;
 
-fn find_command_name_from_derive_input(ast: &syn::DeriveInput) -> String {
+fn find_command_name_from_derive_input(ast: &syn::DeriveInput) -> Option<String> {
 	let attrs = &ast.attrs;
 
 	for attr in attrs {
-		// Make sure this attribute is the kind we're looking for
 		if let syn::MetaItem::NameValue(ref name, ref value) = attr.value {
 			if name != "CommandName" {
 				continue;
 			}
-			// And then we only want string values here.
-			if let &syn::Lit::Str(ref name, _) = value {
-				return name.to_string();
+			if let &syn::Lit::Str(ref string, _) = value {
+				return Some(string.to_string())
 			}
 		}
 	}
@@ -23,14 +21,12 @@ fn find_command_name_from_derive_input(ast: &syn::DeriveInput) -> String {
 pub fn impl_base_command_on(ast: &syn::DeriveInput) -> quote::Tokens {
 	let name = &ast.ident;
 	let command_name = find_command_name_from_derive_input(ast);
-
 	if let syn::Body::Struct(_) = ast.body {
 		quote! {
 			impl BaseCommand for #name {
-				fn on(arguments: Vec<&str>, handler: Box<Fn(Vec<String>, String, String, bool) -> Option<String>>) -> String {
+				fn on(arguments: Vec<&str>, handler: Box<Fn(Vec<String>, String, String, bool) -> Option<String>>) {
 					println!("Woah we're a thing! {:?} {}", arguments, stringify!(#name));
 					handler(vec! [String::from("Test")], "testing".to_string(), "channel".to_string(), true);
-					command_name
 				}
 			}
 		}
